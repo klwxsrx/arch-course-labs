e0fc06efc764b36f14a041c3fb5caa2839def5d5
Viacheslav Bartsev <klwxsrx@icloud.com>
2022-01-27 19:48:48 +0300

    Добавил также инструкцию для деплоя

--- 02-user-service/k8s/README.sh
+++ 02-user-service/k8s/README.sh
@@ -1,2 +1,5 @@
 # Install mysql from helm before:
 helm install --set "image.tag=8.0" --set "auth.database=user" --set "auth.username=user" --set "auth.password=test1234" userservice-db bitnami/mysql
+
+# Apply k8s manifests
+kubectl apply -f config.yaml && kubectl apply -f initdb.yaml && kubectl apply -f .
